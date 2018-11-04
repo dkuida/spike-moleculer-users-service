f20b1fc3e30f916ac48e7a73faacdb08e4c88c00
dkuida <dan@kuida.org>
2018-11-04 04:16:14 +0100

    enable health check

--- services/users.service.js
+++ services/users.service.js
@@ -36,8 +36,6 @@ module.exports = {
 			overallCalls++;
 			if (DIE_AFTER_CALLS > 0 && overallCalls >= DIE_AFTER_CALLS) {
 				this.logger.error(`killing service ${this.fullName} worker ${process.pid} after ${overallCalls} calls`);
-				// await ctx.broker.destroyService(this);
-				await ctx.broker.stop();
 				process.exit(-1);
 			}
 		}
